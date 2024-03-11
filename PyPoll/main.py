d35936937fd639d21a62745c0a54351c10f99234
Tsetsen Purevdorj <124546774+Kai-0322@users.noreply.github.com>
2024-03-11 14:38:10 -0600

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,6 +7,8 @@ file = r"C:\Users\KANO\Desktop\Bootcamp files\weekly challenge 3\python-challang
 id = []
 candidates = []
 unique_names = []
+each_votes = []
+percentages = []
 
 # Open and read csv
 with open(file) as csv_file:
@@ -27,7 +29,37 @@ with open(file) as csv_file:
     for i in range(len(candidates) - 1):
         if candidates[i + 1] != candidates[i] and candidates[i + 1] not in unique_names:
             unique_names.append(candidates[i + 1])
-    
-    print(unique_names)
 
-    print(len(id))
+    # Iterate through candidates to get each votes and associated percentages           
+    for j in range(len(unique_names)):
+        each_votes.append(candidates.count(unique_names[j]))
+        percentages.append(round((each_votes[j] / len(id)) * 100, 3))
+    
+    # Find winner's name
+    winner_name = each_votes.index(max(each_votes))
+
+print("Election Results")
+print("------------------")
+print("Total Votes: " + str(len(id)))
+print("------------------")
+for k in range(len(unique_names)):
+    print(f"{unique_names[k]}: {percentages[k]}% ({each_votes[k]})")
+print("------------------")
+print(f"Winner: {unique_names[winner_name]}")
+print("------------------")
+
+# Set variable for output file
+output_file = os.path.join("python-challange","PyPoll", "analysis", "election_analysis.txt")
+
+#  Open the output file
+with open(output_file, "w", newline='') as datafile:
+
+    datafile.write("Election Results" + "\n")
+    datafile.write("------------------""\n")
+    datafile.write("Total Votes: " + str(len(id)) + "\n")
+    datafile.write("------------------"+ "\n")
+    for x in range(len(unique_names)):
+        datafile.write(f"{unique_names[x]}: {percentages[x]}% ({each_votes[x]})\n")
+    datafile.write("------------------" + "\n")
+    datafile.write(f"Winner: {unique_names[winner_name]}\n")
+    datafile.write("------------------")
